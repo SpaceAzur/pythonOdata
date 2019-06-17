d172f029a66cbd98d96e191b981852915da8ef24
SpaceAzur <a.escobar@hotmail.fr>
2019-06-17 10:49:01 +0200

    croise les doigts

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,18 @@
 
-def salut():
-    return "Hello World"
+from flask import Flask
+import os
 
-fc = salut()
-print(salut())
+
+app = Flask(__name__)
+
+port = int(os.getenv("PORT", 9009))
+
+@app.route('/')
+def hello_world():
+    return "Hello SAP World "
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
+
+
+
