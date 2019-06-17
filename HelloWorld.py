ad9c15ea162f93dea6ea04465fcda49416e34f31
SpaceAzur <a.escobar@hotmail.fr>
2019-06-17 14:40:39 +0200

    suite des tests

--- HelloWorld.py
+++ HelloWorld.py
@@ -9,10 +9,15 @@ port = int(os.getenv("PORT", 9009))
 
 @app.route('/')
 def hello_world():
-    return "Hello SAP World "
+    liste = []
+    t = 0
+    while t < 10:
+        liste.append("coucou"+str(t+1))
+        t=t+1
+
+    return liste
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
-
 
 
